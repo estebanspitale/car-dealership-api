ad4e1649f5b0a044487bb9c1dd5a0369fc2268e6
Esteban Spitale <estebanspitale089@gmail.com>
2026-01-28 20:21:22 -0300

    changes in validation users

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,14 @@
-import express from 'express';
+import express from 'express'
 import {
   getAllUsers,
   getUserById,
   createUser,
   updateUser,
   deleteUser
-} from '../controllers/usersController.js';
+} from '../controllers/usersController.js'
+import { validateUser } from '../middleware/validateUser.js'
 
-import { validateUser } from '../middleware/validateUser.js';
-
-const router = express.Router();
+const router = express.Router()
 
 /**
  * @swagger
@@ -24,11 +23,14 @@ const router = express.Router();
  *       properties:
  *         name:
  *           type: string
+ *           example: Juan Pérez
  *         email:
  *           type: string
+ *           example: juan@email.com
  *         role:
  *           type: string
  *           enum: [admin, seller, customer]
+ *           example: seller
  */
 
 /**
@@ -36,42 +38,99 @@ const router = express.Router();
  * /users:
  *   get:
  *     summary: Get all users
+ *     tags: [Users]
  *     responses:
  *       200:
  *         description: List of users
  */
-router.get('/', getAllUsers);
+router.get('/', getAllUsers)
 
 /**
  * @swagger
  * /users/{id}:
  *   get:
  *     summary: Get user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User found
+ *       404:
+ *         description: User not found
  */
-router.get('/:id', getUserById);
+router.get('/:id', getUserById)
 
 /**
  * @swagger
  * /users:
  *   post:
  *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       201:
+ *         description: User created
+ *       400:
+ *         description: Validation error
  */
-router.post('/', validateUser, createUser);
+router.post('/', validateUser, createUser)
 
 /**
  * @swagger
  * /users/{id}:
  *   put:
  *     summary: Update a user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User updated
+ *       400:
+ *         description: Validation error
+ *       404:
+ *         description: User not found
  */
-router.put('/:id', validateUser, updateUser);
+router.put('/:id', validateUser, updateUser)
 
 /**
  * @swagger
  * /users/{id}:
  *   delete:
  *     summary: Delete a user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: User deleted
+ *       404:
+ *         description: User not found
  */
-router.delete('/:id', deleteUser);
+router.delete('/:id', deleteUser)
 
-export default router;
+export default router
